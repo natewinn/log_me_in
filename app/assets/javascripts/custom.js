28877fdf23f4acf44b7ad1aad29ae7f157d87f62
Nate Winn <nathan.winn@gmail.com>
2014-05-15 16:01:30 -0400

    updated css and js

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,17 +1,21 @@
-$(function(){
+// $(function(){
 
-	$('.text-box').on('input', function() {
-	    var input=$(this);
-	    var is_name=input.val();
-	    if(is_name){input.removeClass("invalid").addClass("valid");}
-	    else{input.removeClass("valid").addClass("invalid");}
-	});
+// 	$('.text-box').on('input', function() {
+// 	    var input=$(this);
+// 	    var is_name=input.val();
+// 	    if(is_name){
+// 	    	input.removeClass("invalid").addClass("valid");
+// 	    }
+// 	    else{
+// 	    	input.removeClass("valid").addClass("invalid");
+// 	    }
+// 	});
 
-	var box = $('#box');  
+// 	var box = $('#box');  
 	  
-	$('toggle').on('click', function () {  
-	  box.toggleClass('hidden');
+// 	$('toggle').on('click', function () {  
+// 	  box.toggleClass('hidden');
 
-	}); 																																														
+// 	}); 																																														
 
-});
+// });
